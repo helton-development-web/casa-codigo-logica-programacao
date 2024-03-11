e540aceee885c5724ff2ea00942affdb253d5099
Helton-Dev <45895791+helton-development-web@users.noreply.github.com>
2024-03-11 01:59:29 -0300

    Add files via upload

--- 19-retorna_primeiro_nome.js
+++ 19-retorna_primeiro_nome.js
@@ -1,8 +1,10 @@
-let userName = getFirstName("Carlos Amoedo Sampaio Filho");
-
+let userName = getFirstName("Carlos-Amoedo-Sampaio-Filho", "-");
 console.log("Seja bem vindo " + userName);
 
-function getFirstName(name){
-    let firstName = name.split(" ")[0];
+userName = getFirstName("Felipe Silva Han Solo"," ");
+console.log("Seja bem vindo" + userName);
+
+function getFirstName(name,splitChar){
+    let firstName = name.split(splitchar)[0];
     return firstName;
 }
